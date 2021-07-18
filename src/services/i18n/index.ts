e99f90a216da17c737ab0b9293af651be18cb9eb
amwebexpert <amwebexpert@gmail.com>
2021-07-18 17:15:23 -0400

    chore: added i18n support

--- src/services/i18n/index.ts
+++ src/services/i18n/index.ts
@@ -1,30 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-const resources = {
-  en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-    },
-  },
-  fr: {
-    translation: {
-      'Welcome to React': 'Bienvenue à React et react-i18next',
-    },
-  },
-};
+import translations from './translations';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
+    resources: translations,
+    lng: 'en', // language to use
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
---
+++ src/services/i18n/translations/fr/index.ts
@@ -0,0 +1,5 @@
+export default {
+  translation: {
+    'Welcome to React': "Bienvenue dans l'application !",
+  },
+};
