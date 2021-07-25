a395cd0b65571318d9ba9b69c00907df1159bef9
Andre Masson <amwebexpert@gmail.com>
2021-07-25 17:29:26 -0400

    chore: make build work on iOS

---
+++ ios/Devine-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef Devine_Bridging_Header_h
+#define Devine_Bridging_Header_h
+
+#import "RNSplashScreen.h" // here
+
+#endif /* Devine_Bridging_Header_h */
---
+++ ios/Devine/Devine-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//  Devine-Bridging-Header.h
+
+#ifndef Devine_Bridging_Header_h
+#define Devine_Bridging_Header_h
+
+#import "RNSplashScreen.h" // here
+
+#endif /* Devine_Bridging_Header_h */
